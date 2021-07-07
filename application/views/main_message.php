77cf7173cb4c5aadd37ad35cec06e47a4a4d2407
hankiwoo <hankiwoo17@gmail.com>
2021-07-07 15:59:20 +0900

    test

--- application/views/main_message.php
+++ application/views/main_message.php
@@ -2,3 +2,6 @@
     assign
     <input type="text" name="" value="<?=$test?>">
 </div>
+<div>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque sequi quisquam, corporis praesentium blanditiis. Ullam est tempora repellat aperiam earum laudantium, dolore doloribus vel rerum aut fuga quis dolorum, saepe.
+</div>
